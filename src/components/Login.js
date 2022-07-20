5067a98d7350edddd5242c317b7c983d4352aeed
Harshad <rharshad26@gmail.com>
2022-07-20 13:56:05 +0530

    ADD: images and videos folder

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,31 @@ import styled from 'styled-components';
 
 const Login = (props) => {
   return (
-    <div>login</div>
+    <Container>
+      <Content>Content</Content>
+    </Container>
   )
 };
+
+const Container = styled.section`
+  overflow: hidden;
+  display:flex;
+  flex-direction: column:
+  width: 100vw;
+  justify-content: center;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10vw;
+  min-height: 100vh;
+  position: relative;
+  box-sizing: border-box;
+  height: 100%;
+  padding: 80px 40px;
+`
 
 export default Login;
